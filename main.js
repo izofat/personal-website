36da230b97b9cfcf057ac01034181c6866ae28a0
Gorkem Kacar <139143704+izofat@users.noreply.github.com>
2023-11-11 00:04:50 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+function goToGithub(){
+    window.location = "https://github.com/izofat";
+}
+function goToLinkedin(){
+    window.location = "https://www.linkedin.com/in/g%C3%B6rkem-ka%C3%A7ar-416312196/";
+}
